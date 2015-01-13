136d218cd351ac1183507a56eeaf83a378b8bc39
Arvinder Palaha <Arvinder.Palaha@diamond.ac.uk>
2015-01-13 19:11:46 +0000

    Added getter methods to give source and release directories. Tidied some of the docstrings.

--- vcs.py
+++ vcs.py
@@ -8,31 +8,47 @@ class BaseVCS(object):
 
     @abc.abstractmethod
     def check_epics_version(self, src_dir, build_epics, epics_version):
-        ''' Compare epics version on machine and requested, confirm choice '''
+        ''' Compare epics version on machine and requested, confirm choice. '''
         raise NotImplementedError
 
 
     @abc.abstractmethod
     def next_release(self, module, area, options):
-        ''' Work out the release number by checking source directory '''
+        ''' Work out the release number by checking source directory. '''
         raise NotImplementedError
 
 
     @abc.abstractmethod
-    def pathcheck(self, path):
-        ''' search for path '''
+    def path_check(self, path):
+        ''' search for path. '''
         raise NotImplementedError
 
 
     @abc.abstractmethod
-    def create_release(self):
-        ''' create release/tag of module '''
+    def checkout_module(self):
+        ''' Create release/tag of module. '''
         raise NotImplementedError
 
 
     @abc.abstractmethod
-    def setLogMessage(self, message):
+    def set_log_message(self, message):
         '''
-        abstraction for callback function to return message string for log
+        Abstraction for callback function to return message string for log.
+        '''
+        raise NotImplementedError
+
+
+    @abc.abstractmethod
+    def get_src_dir(self, module, options):
+        '''
+        Find/create the source directory from which to release the module.
+        '''
+        raise NotImplementedError
+
+
+    @abc.abstractmethod
+    def get_rel_dir(self, module, options):
+        '''
+        Create the release directory the module will be released into.
         '''
         raise NotImplementedError
