6aa8b83f3828c0a9bcbb728f33145079c8c075aa
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-11-12 22:00:26 +0000

    Modified setup.py to match this new module

--- setup.py
+++ setup.py
@@ -8,26 +8,18 @@ version = os.environ.get("MODULEVER", "0.0")
 script_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "py_scripts"))
 entry_points = [ x.replace('_','-') + " = py_scripts." + x[:-3] + ":"+x.replace('dls_','')[:-3] 
                     for x in os.listdir(script_dir) if x.endswith(".py") and x != "__init__.py" ]
-install_requires = [
-    'dls_environment==4.6',
-    'dls_release==1.1',
-    'cx_Oracle==5.0.4',
-    'dls_serial_sim==1.17',
-    'iocbuilder==3.40',
-    'python-ldap==2.3.12',
-    'numpy==1.6.2',
-    'cothread==2.8']
+install_requires = []
 
 setup(
     # name of the module
-    name = "dls_scripts",
+    name = "dls_ade",
     # version: over-ridden by the release script
     version = version,
-    description = 'Various Diamond helper scripts',
+    description = 'DLS Controls Group Application Development Environment scripts',
     author = 'Diamond Light Source Controls Group',
     install_requires = install_requires,
-    packages = ["py_scripts","dls_scripts"],
-    package_data = {'dls_scripts' : ["def_config.cfg", "COPYING", "COPYING.LESSER"] },
+    packages = ["py_scripts","dls_environment"],
+    package_data = {},
     # define console_scripts
     entry_points = { 'console_scripts': entry_points },
     zip_safe = False
