1988ddadfef84595d40c53ec1b52a06dcef709a0
GDYendell <gary.yendell@diamond.ac.uk>
2015-11-20 14:35:50 +0000

    small formatting changes

--- dls_ade/vcs_git.py
+++ dls_ade/vcs_git.py
@@ -5,9 +5,10 @@ import git
 import tempfile
 import subprocess
 
+GIT_ROOT = "ssh://dascgitolite@dasc-git.diamond.ac.uk/"
+
 
 class Git(BaseVCS):
-
 
     def __init__(self, module, options):
 
@@ -16,28 +17,23 @@ class Git(BaseVCS):
         self.init_client()
         self._version = None
 
-
     @property
     def vcs_type(self):
         return 'git'
-
 
     @property
     def module(self):
         return self._module
 
-
     @property
     def source_repo(self):
         return self._remote_repo
-
 
     @property
     def version(self):
         if not self._version:
             raise Exception('version not set')
         return self._version
-
 
     def init_client(self):
         list_cmd = 'ssh dascgitolite@dasc-git.diamond.ac.uk expand controls'
@@ -47,12 +43,11 @@ class Git(BaseVCS):
         if server_repo_path not in list_cmd_output:
             raise Exception('repo not found on gitolite server')
 
-        repo_dir = tempfile.mkdtemp(suffix="_" + self._module.replace("/","_"))
+        repo_dir = tempfile.mkdtemp(suffix="_" + self._module.replace("/", "_"))
         self._remote_repo = 'ssh://dascgitolite@dasc-git.diamond.ac.uk/'
         self._remote_repo += server_repo_path
 
         self.client = git.Repo.clone_from(self._remote_repo, repo_dir)
-
 
     def cat(self, filename):
         '''
@@ -63,8 +58,7 @@ class Git(BaseVCS):
         if self._version:
             if self.check_version_exists(self._version):
                 tag = self._version
-        return self.client.git.cat_file('-p', tag+':'+filename)
-
+        return self.client.git.cat_file('-p', tag + ':' + filename)
 
     def list_releases(self):
         '''Return list of release tags of module.'''
@@ -74,29 +68,23 @@ class Git(BaseVCS):
                 self.releases.append(tag.name)
         return self.releases
 
-
     def set_log_message(self, message):
         '''Git support will not do a commit, so log message not needed.'''
         return None
 
-
     def check_version_exists(self, version):
         return version in self.list_releases()
 
-
     def set_branch(self, branch):
         raise NotImplementedError('branch handling for git not implemented')
-
 
     def set_version(self, version):
         if not self.check_version_exists(version):
             raise Exception('version does not exist')
         self._version = version
 
-
     def release_version(self, version):
         raise NotImplementedError('version release for git not implemented')
-
 
 # sanity check: ensure class fully implements the interface (abc)
 assert issubclass(Git, BaseVCS), "Git is not a base class of BaseVCS"
