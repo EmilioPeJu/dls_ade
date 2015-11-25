976e3667195ff5c1a2416c6470d6462af8fc71a9
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2015-11-25 16:17:15 +0000

    Initial start_new_module tests included

--- dls_ade/dls_start_new_module_test.py
+++ dls_ade/dls_start_new_module_test.py
@@ -5,32 +5,47 @@ import dls_start_new_module
 from pkg_resources import require
 require("mock")
 from mock import patch, ANY, MagicMock, mock_open
+from new_module_templates import py_files, tools_files
 
 from sys import version_info
 if version_info.major == 2:
-    import __builtin__ as builtins # Allows for Python 2/3 compatibility
+    import __builtin__ as builtins  # Allows for Python 2/3 compatibility
 else:
     import builtins
 
 
 class MakeFilesPythonTest(unittest.TestCase):
 
+    def setUp(self):
+        self.test_input = {"module": "test_module_name", "getlogin": "test_login_name"}
+
     def test_setup_opens_correct_file(self):
 
-        open_mock = mock_open() # Mocks the 'open' command
+        module = self.test_input['module']
+        getlogin = self.test_input['getlogin']
 
-        with patch.object(builtins, 'open', open_mock):
-            dls_start_new_module.make_files_python("test_module_name")
+        open_mock = mock_open()  # mock_open specifically designed to mock the 'open' function
+
+        with patch.object(builtins, 'open', open_mock):  # 'builtins' is namespace for inbuilt functions
+            dls_start_new_module.make_files_python(module)
 
         open_mock.assert_called_once_with("setup.py", "w")
 
-    @patch('dls_start_new_module.os.getlogin', return_value="TestLogin")
-    def test_setup_prints_correctly(self, mock_login):
+    def test_setup_prints_correctly(self):
 
-        open_mock = mock_open() # Mocks the 'open' command
+        module = self.test_input['module']
+        getlogin = self.test_input['getlogin']
+
+        mock_login = patch('dls_start_new_module.os.getlogin', return_value=self.test_input['getlogin'])
+
+        open_mock = mock_open()
+        file_handle_mock = open_mock()  # Allows us to mock the 'write' function
 
         with patch.object(builtins, 'open', open_mock):
-            dls_start_new_module.make_files_python("testmodulename")
+            with mock_login:
+                dls_start_new_module.make_files_python(module)
+
+        file_handle_mock.write.assert_called_once_with(py_files['setup.py'] % (module, getlogin, getlogin, module, module, module))
 
 
 
