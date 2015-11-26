15cb4750c9d64c047f956a88160a26abccd0660c
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2015-11-26 13:16:34 +0000

    Added MakeParserTest to dls_start_new_module_test.py

--- dls_ade/dls_start_new_module_test.py
+++ dls_ade/dls_start_new_module_test.py
@@ -6,14 +6,37 @@ from pkg_resources import require
 require("mock")
 from mock import patch, ANY, MagicMock, mock_open
 from new_module_templates import py_files, tools_files
-import new_module_format_templates as new_t
 import os
+from argparse import _StoreTrueAction
 
 from sys import version_info
 if version_info.major == 2:
     import __builtin__ as builtins  # Allows for Python 2/3 compatibility, 'builtins' is namespace for inbuilt functions
 else:
     import builtins
+
+
+class MakeParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = dls_start_new_module.make_parser()
+
+    def test_module_name_has_correct_attributes(self):
+        arguments = self.parser._positionals._actions[4]
+        self.assertEqual(arguments.type, str)
+        self.assertEqual(arguments.dest, 'module_name')
+
+    def test_branch_argument_has_correct_attributes(self):
+        option = self.parser._option_string_actions['-n']
+        self.assertIsInstance(option, _StoreTrueAction)
+        self.assertEqual(option.dest, "no_import")
+        self.assertIn("--no_import", option.option_strings)
+
+    def test_force_argument_has_correct_attributes(self):
+        option = self.parser._option_string_actions['-f']
+        self.assertIsInstance(option, _StoreTrueAction)
+        self.assertEqual(option.dest, "fullname")
+        self.assertIn("--fullname", option.option_strings)
 
 
 class MakeFilesPythonTest(unittest.TestCase):
