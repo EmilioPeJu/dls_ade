13326c76d03e1579cf19a40e56369824d2df5db6
GDYendell <gary.yendell@diamond.ac.uk>
2015-12-15 09:08:25 +0000

    partially converted to git

--- dls_ade/dls_changes_since_release.py
+++ dls_ade/dls_changes_since_release.py
@@ -2,33 +2,36 @@
 # This script comes from the dls_scripts python module
 
 import sys
+import os
+import shutil
+import logging
 from argument_parser import ArgParser
 from dls_environment import environment
 import path_functions as path
+import vcs_git
+from pkg_resources import require
+require('GitPython')
+import git
 
-usage = """Default <area> is 'support'.
-Check if a module in the <area> area of the repository has had changes committed since its last release."""
+e = environment()
+logging.basicConfig(level=logging.DEBUG)
+
+usage = """
+Default <area> is 'support'.
+Check if a module in the <area> area of the repository has had changes committed since its last release.
+"""
 
 
 def make_parser():
-    # parse options
+
     parser = ArgParser(usage)
-    parser.add_argument("module_name", type=str,
-                        help="name of module to release")
+    parser.add_argument("module_name", type=str, help="name of module to release")
     return parser
 
 
-def check_parsed_arguments_valid(args, parser):
-
-    if 'module_name' not in args:
-        parser.error("Module name required")
-
-
 def check_technical_area_valid(args, parser):
-    module = args['module']
-    area = args['area']
 
-    if area == "ioc" and not len(module.split('/')) > 1:
+    if args.area == "ioc" and not len(args.module.split('/')) > 1:
         parser.error("Missing Technical Area Under Beamline")
 
 
@@ -37,33 +40,39 @@ def main():
     parser = make_parser()
     args = parser.parse_args()
 
-    check_parsed_arguments_valid(vars(args), parser)
-    check_technical_area_valid(vars(args), parser)
-
-    # setup the environment
-    e = environment()
+    check_technical_area_valid(args, parser)
 
     module = args.module_name
 
     source = path.devModule(module, args.area)
+    logging.debug(source)
     release = path.prodModule(module, args.area)
-                
+    logging.debug(release)
+
     # Check for existence of this module in various places in the repository and note revisions
-    assert svn.pathcheck(source), "Repository does not contain '" + source + "'"
+    if not vcs_git.is_repo_path(source):
+        parser.error("Repository does not contain " + source)
 
-    last_trunk_rev = svn.info2(source, recurse=False)[0][1]["last_changed_rev"].number
+    if vcs_git.is_repo_path(source):
+        if os.path.isdir('./' + module):
+            repo = vcs_git.git.Repo(module)
+            last_trunk_rev = vcs_git.list_module_releases(git.Repo(repo))
+        else:
+            vcs_git.clone(source, module)
+            repo = vcs_git.git.Repo(module)
+            last_trunk_rev = vcs_git.list_module_releases(git.Repo(repo))
+            shutil.rmtree(module)
 
-    if svn.pathcheck(release):
-        last_release_rev = \
-            svn.info2(release, recurse=False)[0][1]["last_changed_rev"].number
-        last_release_num = \
-            e.sortReleases([x["name"] for x in svn.ls(release)])[-1].split("/")[-1]
+    if vcs_git.is_repo_path(release):
+        last_release_rev = "last rev"
+        # svn.info2(release, recurse=False)[0][1]["last_changed_rev"].number
+        last_release_num = "last num"
+        # e.sortReleases([x["name"] for x in svn.ls(release)])[-1].split("/")[-1]
         # print the output
         if last_trunk_rev > last_release_rev:
-            print(module + " (" + last_release_num + \
-                  "): Outstanding changes. Release = r" + \
-                  str(last_release_rev) + ", Trunk = r" + \
-                  str(last_trunk_rev))
+            print(module + " (" + last_release_num + "): Outstanding changes. "
+                                                     "Release = r" + str(last_release_rev) +
+                                                     ",Trunk = r" + str(last_trunk_rev))
         else:
             print(module + " (" + last_release_num + "): Up to date.")
     else:
