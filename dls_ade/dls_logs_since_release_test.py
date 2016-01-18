a1eb94680348a2142f752838622535ee183352ba
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-18 13:24:36 +0000

    Refactor create_release_list test

--- dls_ade/dls_logs_since_release_test.py
+++ dls_ade/dls_logs_since_release_test.py
@@ -69,12 +69,18 @@ class CreateReleaseListTest(unittest.TestCase):
 
         repo_inst = MagicMock()
         dls_logs_since_release.vcs_git.git.repo = repo_inst
+        tag_inst_1 = MagicMock()
+        tag_inst_1.name = '1-0'
+        tag_inst_2 = MagicMock()
+        tag_inst_2.name = '2-0'
+        tag_inst_3 = MagicMock()
+        tag_inst_3.name = '2-1'
 
-        repo_inst.tags = ['1-0', '1-1', '1-2', '2-0', '2-1', '3-0']
+        repo_inst.tags = [tag_inst_1, tag_inst_2, tag_inst_3]
 
         releases = dls_logs_since_release.create_release_list(repo_inst)
 
-        self.assertEqual(releases, ['1-0', '1-1', '1-2', '2-0', '2-1', '3-0'])
+        self.assertEqual(releases, ['1-0', '2-0', '2-1'])
 
     def test_given_repo_with_no_tags_then_empty_list_returned(self):
         repo_inst = MagicMock()
