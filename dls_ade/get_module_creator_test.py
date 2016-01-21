556d8ab2042ac1fa479bbe7b64992e04203b6918
Martin Gaughran <martin.gaughran@diamond.ac.uk>
2016-01-21 15:46:15 +0000

    Refactored get_module_creator function tests for clarity

--- dls_ade/get_module_creator_test.py
+++ dls_ade/get_module_creator_test.py
@@ -9,12 +9,6 @@ from pkg_resources import require
 require("mock")
 from mock import patch, ANY, MagicMock, call
 
-from sys import version_info
-if version_info.major == 2:
-    import __builtin__ as builtins  # Allows for Python 2/3 compatibility, 'builtins' is namespace for inbuilt functions
-else:
-    import builtins
-
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -33,98 +27,21 @@ class GetModuleCreatorTest(unittest.TestCase):
 
     def setUp(self):
 
-        nmc_classes_to_patch = [
-            'Creator',
-            'CreatorWithApps',
-        ]
+        self.git_root_dir = get_mc.vcs_git.GIT_ROOT_DIR
 
-        self.nmc_mocks = {}
-        for cls in nmc_classes_to_patch:
-            self.nmc_mocks[cls] = set_up_mock(self, 'dls_ade.module_creator.Module' + cls)
+        self.mock_nmc_base = set_up_mock(self, 'dls_ade.module_creator.ModuleCreator')
+        self.mock_nmc_base.return_value = "ModuleCreatorBase"
 
-        self.mock_nmc_base = self.nmc_mocks['Creator']
-        self.mock_nmc_with_apps = self.nmc_mocks['CreatorWithApps']
+        self.mock_nmc_with_apps = set_up_mock(self, 'dls_ade.module_creator.ModuleCreatorWithApps')
+        self.mock_nmc_with_apps.return_value = "ModuleCreatorWithApps"
+
+        self.mock_nmc_add_app = set_up_mock(self, 'dls_ade.module_creator.ModuleCreatorAddAppToModule')
+        self.mock_nmc_add_app.return_value = "ModuleCreatorAddApp"
 
         mt_classes_to_patch = [
             'Python',
             'Support',
-            'Tools'
-        ]
-
-        self.mt_mocks = {}
-        for cls in mt_classes_to_patch:
-            self.mt_mocks[cls] = set_up_mock(self, 'dls_ade.module_template.ModuleTemplate' + cls)
-
-        self.mock_get_new_ioc = set_up_mock(self, 'dls_ade.get_module_creator.get_module_creator_ioc')
-
-        # self.mocks['CreatorTools'].return_value = "Example"
-
-    def test_given_unsupported_area_then_exception_raised(self):
-
-        with self.assertRaises(ParsingError):
-            get_mc.get_module_creator("test_module", "fake_area")
-
-    def test_given_area_is_ioc_then_get_module_creator_ioc_called(self):
-
-        new_ioc_creator = get_mc.get_module_creator("test_module", "ioc", fullname=True)
-
-        self.mock_get_new_ioc.assert_called_once_with("test_module", True)
-
-    def test_given_area_is_python_with_invalid_name_then_module_creator_python_not_returned(self):
-
-        with self.assertRaises(ParsingError):
-            new_py_creator = get_mc.get_module_creator("test_module", "python")
-
-        self.assertFalse(self.mock_nmc_base.called)
-
-        with self.assertRaises(ParsingError):
-            new_py_creator = get_mc.get_module_creator("dls_test-module", "python")
-
-        self.assertFalse(self.mock_nmc_base.called)
-
-        with self.assertRaises(ParsingError):
-            new_py_creator = get_mc.get_module_creator("dls_test.module", "python")
-
-        self.assertFalse(self.mock_nmc_base.called)
-
-    def test_given_area_is_python_with_valid_name_then_module_creator_returned_with_correct_args(self):
-
-        new_py_creator = get_mc.get_module_creator("dls_test_module", "python")
-
-        self.mock_nmc_base.assert_called_once_with("dls_test_module", "python", self.mt_mocks['Python'])
-
-    def test_given_area_is_support_then_module_creator_returned_with_correct_args(self):
-
-        new_sup_creator = get_mc.get_module_creator("test_module")  # Area automatically support
-
-        self.mock_nmc_with_apps.assert_called_once_with("test_module", "support", self.mt_mocks['Support'], app_name="test_module")
-
-    def test_given_area_is_tools_then_module_creator_returned_with_correct_args(self):
-
-        new_tools_creator = get_mc.get_module_creator("test_module", "tools")
-
-        self.mock_nmc_base.assert_called_once_with("test_module", "tools", self.mt_mocks['Tools'])
-
-
-class GetModuleCreatorIOCTest(unittest.TestCase):
-
-    def setUp(self):
-
-        self.git_root_dir = get_mc.vcs_git.GIT_ROOT_DIR
-
-        nmc_classes_to_patch = [
-            'CreatorWithApps',
-            'CreatorAddAppToModule'
-        ]
-
-        self.nmc_mocks = {}
-        for cls in nmc_classes_to_patch:
-            self.nmc_mocks[cls] = set_up_mock(self, 'dls_ade.module_creator.Module' + cls)
-
-        self.mock_nmc_with_apps = self.nmc_mocks['CreatorWithApps']
-        self.mock_nmc_add_app = self.nmc_mocks['CreatorAddAppToModule']
-
-        mt_classes_to_patch = [
+            'Tools',
             'IOC',
             'IOCBL'
         ]
@@ -134,79 +51,138 @@ class GetModuleCreatorIOCTest(unittest.TestCase):
             self.mt_mocks[cls] = set_up_mock(self, 'dls_ade.module_template.ModuleTemplate' + cls)
 
         self.mock_is_repo_path = set_up_mock(self, 'dls_ade.vcs_git.is_repo_path')
-        self.mock_split_ioc_module_name = set_up_mock(self, 'dls_ade.get_module_creator.split_ioc_module_name')
 
         # self.mocks['CreatorTools'].return_value = "Example"
 
-    def test_given_module_name_with_invalid_separators_then_exception_raised_with_correct_message(self):
 
-        self.mock_split_ioc_module_name.side_effect = ParsingError("error_message")
+class GetModuleCreatorTestBase(GetModuleCreatorTest):
+
+    def test_given_unsupported_area_then_exception_raised(self):
+
+        with self.assertRaises(ParsingError) as e:
+            get_mc.get_module_creator("test_module", "fake_area")
+
+        self.assertTrue("fake_area" in str(e.exception))
+
+
+class GetModuleCreatorTestPython(GetModuleCreatorTest):
+
+    def test_given_area_is_python_with_invalid_name_then_module_creator_python_not_returned(self):
+
+        with self.assertRaises(ParsingError) as e:
+            new_py_creator = get_mc.get_module_creator("test_module", "python")
+
+        self.assertFalse(self.mock_nmc_base.called)
+
+        with self.assertRaises(ParsingError) as e:
+            new_py_creator = get_mc.get_module_creator("dls_test-module", "python")
+
+        self.assertFalse(self.mock_nmc_base.called)
+
+        with self.assertRaises(ParsingError) as e:
+            new_py_creator = get_mc.get_module_creator("dls_test.module", "python")
+
+        self.assertFalse(self.mock_nmc_base.called)
+
+    def test_given_area_is_python_with_valid_name_then_module_creator_returned_with_correct_args(self):
+
+        new_py_creator = get_mc.get_module_creator("dls_test_module", "python")
+
+        self.assertEqual(new_py_creator, "ModuleCreatorBase")
+        self.mock_nmc_base.assert_called_once_with("dls_test_module", "python", self.mt_mocks['Python'])
+
+class GetModuleCreatorTestTools(GetModuleCreatorTest):
+
+    def test_given_area_is_tools_then_module_creator_returned_with_correct_args(self):
+
+        new_tools_creator = get_mc.get_module_creator("test_module", "tools")
+
+        self.assertEqual(new_tools_creator, "ModuleCreatorBase")
+        self.mock_nmc_base.assert_called_once_with("test_module", "tools", self.mt_mocks['Tools'])
+
+
+class GetModuleCreatorTestSupport(GetModuleCreatorTest):
+
+    def test_given_area_is_support_then_module_creator_returned_with_correct_args(self):
+
+        new_sup_creator = get_mc.get_module_creator("test_module")  # Area automatically support
+
+        self.assertEqual(new_sup_creator, "ModuleCreatorWithApps")
+        self.mock_nmc_with_apps.assert_called_once_with("test_module", "support", self.mt_mocks['Support'], app_name="test_module")
+
+
+class GetModuleCreatorTestIOC(GetModuleCreatorTest):
+
+    def test_given_module_name_with_invalid_separators_then_exception_raised_with_correct_message(self):
 
         with self.assertRaises(ParsingError) as e:
             new_ioc_creator = get_mc.get_module_creator_ioc("test_module")
 
-        self.assertEqual(str(e.exception), "error_message")
+        self.assertTrue("test_module" in str(e.exception))
 
     def test_given_not_BL_and_dash_separated_then_module_creator_with_apps_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = True, ["test", "module", "IOC", "01"]
-
         new_ioc_creator = get_mc.get_module_creator_ioc("test-module-IOC-01")
 
+        self.assertEqual(new_ioc_creator, "ModuleCreatorWithApps")
         self.mock_nmc_with_apps.assert_called_once_with("test/test-module-IOC-01", "ioc", self.mt_mocks['IOC'], app_name="test-module-IOC-01")
 
     def test_given_area_is_ioc_and_not_BL_and_slash_separated_with_fullname_true_then_module_creator_with_apps_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = False, ["test", "module", "02"]
-
         new_ioc_creator = get_mc.get_module_creator_ioc("test/module/02", fullname=True)
 
+        self.assertEqual(new_ioc_creator, "ModuleCreatorWithApps")
         self.mock_nmc_with_apps.assert_called_once_with("test/test-module-IOC-02", "ioc", self.mt_mocks['IOC'], app_name="test-module-IOC-02")
 
     def test_given_area_is_ioc_and_not_BL_and_slash_separated_with_fullname_true_but_no_ioc_number_then_module_creator_with_apps_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = False, ["test", "module"]
-
         new_ioc_creator = get_mc.get_module_creator_ioc("test/module", fullname=True)
 
+        self.assertEqual(new_ioc_creator, "ModuleCreatorWithApps")
         self.mock_nmc_with_apps.assert_called_once_with("test/test-module-IOC-01", "ioc", self.mt_mocks['IOC'], app_name="test-module-IOC-01")
 
     def test_given_area_is_ioc_and_not_BL_and_slash_separated_with_fullname_false_and_module_path_not_in_remote_repo_then_module_creator_with_apps_returned_with_correct_args(self):
-
-        self.mock_split_ioc_module_name.return_value = False, ["test", "module", "01"]
 
         self.mock_is_repo_path.return_value = False
 
         new_ioc_creator = get_mc.get_module_creator_ioc("test/module/01", fullname=False)
 
+        self.assertEqual(new_ioc_creator, "ModuleCreatorWithApps")
         self.mock_is_repo_path.assert_called_once_with(self.git_root_dir+"/ioc/test/module")
         self.mock_nmc_with_apps.assert_called_once_with("test/module", "ioc", self.mt_mocks['IOC'], app_name="test-module-IOC-01")
 
     def test_given_area_is_ioc_and_not_BL_and_slash_separated_with_fullname_false_and_module_path_in_remote_repo_then_module_creator_add_to_module_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = False, ["test", "module", "02"]
-
         self.mock_is_repo_path.return_value = True
 
         new_ioc_creator = get_mc.get_module_creator_ioc("test/module/02", fullname=False)
 
+        self.assertEqual(new_ioc_creator, "ModuleCreatorAddApp")
         self.mock_is_repo_path.assert_called_once_with(self.git_root_dir+"/ioc/test/module")
         self.mock_nmc_add_app.assert_called_once_with("test/module", "ioc", self.mt_mocks['IOC'], app_name="test-module-IOC-02")
 
+
+class GetModuleCreatorTestIOCBL(GetModuleCreatorTest):
+
+    def test_given_module_name_with_invalid_separators_then_exception_raised_with_correct_message(self):
+
+        with self.assertRaises(ParsingError) as e:
+            new_ioc_bl_creator = get_mc.get_module_creator_ioc("test_BL_module")
+
+        self.assertTrue("test_BL_module" in str(e.exception))
+
     def test_given_area_is_ioc_and_tech_area_is_BL_slash_form_then_module_creator_with_apps_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = False, ["test", "BL"]
+        new_ioc_bl_creator = get_mc.get_module_creator_ioc("test/BL")
 
-        new_tools_creator = get_mc.get_module_creator_ioc("test/BL")
-
+        self.assertEqual(new_ioc_bl_creator, "ModuleCreatorWithApps")
         self.mock_nmc_with_apps.assert_called_once_with("test/BL", "ioc", self.mt_mocks['IOCBL'], app_name="test")
 
     def test_given_area_is_ioc_and_tech_area_is_BL_dash_form_then_module_creator_with_apps_returned_with_correct_args(self):
 
-        self.mock_split_ioc_module_name.return_value = True, ["test", "BL", "IOC", "01"]
+        new_ioc_bl_creator = get_mc.get_module_creator_ioc("test-BL-IOC-01", "ioc")
 
-        new_tools_creator = get_mc.get_module_creator_ioc("test-BL-IOC-01", "ioc")
-
+        self.assertEqual(new_ioc_bl_creator, "ModuleCreatorWithApps")
         self.mock_nmc_with_apps.assert_called_once_with("test/test-BL-IOC-01", "ioc", self.mt_mocks['IOCBL'], app_name="test-BL-IOC-01")
 
 
