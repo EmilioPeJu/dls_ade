1b78af26bf093e2d679d582eb6ed0eb5c4c50ff8
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-22 15:02:30 +0000

    Add branch tests

--- systems_testing/checkout_module/checkout_module_sys_test.py
+++ systems_testing/checkout_module/checkout_module_sys_test.py
@@ -19,6 +19,24 @@ settings_list = [
 
     },
 
+    # Checkout one module from support area, change branch and check it is correctly cloned
+    {
+        'description': "checkout_from_support_and_change_branch",
+
+        'arguments': "testsupportmod -b bug-fix",
+
+        'local_repo_path': "testsupportmod",
+
+        'branch_name': "bug-fix",
+
+        'repo_comp_method': "server_comp",
+
+        'local_comp_path_one': "testsupportmod",
+
+        'server_repo_path': "controlstest/support/testsupportmod",
+
+    },
+
     # Checkout one module from python area and check it is correctly cloned
     {
         'description': "checkout_from_python",
@@ -33,6 +51,24 @@ settings_list = [
 
     },
 
+    # # Checkout one module from python area, change branch and check it is correctly cloned
+    # {
+    #     'description': "checkout_from_python_and_change_branch",
+    #
+    #     'arguments': "-p dls_testpythonmod -b bug-fix",
+    #
+    #     'local_repo_path': "dls_testpythonmod",
+    #
+    #     'branch_name': "bug-fix",
+    #
+    #     'repo_comp_method': "server_comp",
+    #
+    #     'local_comp_path_one': "dls_testpythonmod",
+    #
+    #     'server_repo_path': "controlstest/python/dls_testpythonmod",
+    #
+    # },
+
     # Checkout one module from ioc area and check it is correctly cloned
     {
         'description': "checkout_from_ioc",
@@ -46,6 +82,24 @@ settings_list = [
         'server_repo_path': "controlstest/ioc/BTEST/TS",
 
     },
+
+    # # Checkout one module from ioc area, change branch and check it is correctly cloned
+    # {
+    #     'description': "checkout_from_ioc_and_change_branch",
+    #
+    #     'arguments': "-i BTEST/TS -b bug-fix",
+    #
+    #     'local_repo_path': "BTEST/TS",
+    #
+    #     'branch_name': "bug-fix",
+    #
+    #     'repo_comp_method': "server_comp",
+    #
+    #     'local_comp_path_one': "BTEST/TS",
+    #
+    #     'server_repo_path': "controlstest/ioc/BTEST/TS",
+    #
+    # },
 
     # Checkout everything from support area and check one of them is correctly cloned
     {
