291ff07976a5b23331232ebef908858867ca3fc6
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-22 13:36:58 +0000

    Add CSV output test

--- systems_testing/module_contacts/module_contacts_sys_test.py
+++ systems_testing/module_contacts/module_contacts_sys_test.py
@@ -5,6 +5,22 @@ import shutil
 
 sys_test_dir_path = os.path.realpath(os.path.dirname(__file__))
 
+support_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
+                     'Performing search for lkz95212...\n' \
+                     'Performing search for mef65357...\n' \
+                     'testsupportmod,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
+
+python_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
+                     'Performing search for lkz95212...\n' \
+                     'Performing search for mef65357...\n' \
+                     'dls_testpythonmod,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
+
+ioc_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
+                     'Performing search for lkz95212...\n' \
+                     'Performing search for mef65357...\n' \
+                     'BTEST/TS,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
+
+
 settings_list = [
 
     # Check contacts of support module
@@ -14,6 +30,16 @@ settings_list = [
         'arguments': "testsupportmod",
 
         'std_out_compare_string': "Contact: lkz95212 (CC: mef65357)\n",
+
+    },
+
+    # Check contacts of support module in CSV format
+    {
+        'description': "check_support_contacts_CSV",
+
+        'arguments': "testsupportmod -s",
+
+        'std_out_compare_string': support_csv_output,
 
     },
 
@@ -53,6 +79,16 @@ settings_list = [
 
     },
 
+    # Check contacts of python module in CSV format
+    {
+        'description': "check_python_contacts_CSV",
+
+        'arguments': "-p dls_testpythonmod -s",
+
+        'std_out_compare_string': python_csv_output,
+
+    },
+
     # Set contacts of python module
     {
         'description': "set_python_contacts",
@@ -86,6 +122,16 @@ settings_list = [
         'arguments': "-i BTEST/TS",
 
         'std_out_compare_string': "Contact: lkz95212 (CC: mef65357)\n",
+
+    },
+
+    # Check contacts of ioc module CSV format
+    {
+        'description': "check_ioc_contacts",
+
+        'arguments': "-i BTEST/TS -s",
+
+        'std_out_compare_string': ioc_csv_output,
 
     },
 
