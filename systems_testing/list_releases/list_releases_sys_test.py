4a0d60d67d3ac987be9a49b51e1c79fe496cc332
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-25 11:42:24 +0000

    Add tests for python and ioc areas

--- systems_testing/list_releases/list_releases_sys_test.py
+++ systems_testing/list_releases/list_releases_sys_test.py
@@ -1,6 +1,8 @@
 from systems_testing import systems_testing as st
 
 support_list = "Previous releases for testsupportmod in the repository:\n1-0\n1-1\n"
+python_list = "Previous releases for dls_testpythonmod in the repository:\n1-0\n1-1\n"
+ioc_list = "Previous releases for BTEST/TS in the repository:\n1-0\n1-1\n"
 
 settings_list = [
 
@@ -14,42 +16,30 @@ settings_list = [
 
     },
 
-    # # List everything in python area
-    # {
-    #     'description': "list_python",
-    #
-    #     'arguments': "-p",
-    #
-    #     'std_out_compare_string': python_list,
-    #
-    # },
-    #
-    # # List everything in ioc area
-    # {
-    #     'description': "list_ioc",
-    #
-    #     'arguments': "-i",
-    #
-    #     'std_out_compare_string': ioc_list,
-    #
-    # },
-    #
-    # # List everything in an ioc domain
-    # {
-    #     'description': "list_ioc_domain",
-    #
-    #     'arguments': "-i -d BTEST",
-    #
-    #     'std_out_compare_string': ioc_domain_list,
-    #
-    # },
+    # List releases for python module on repo
+    {
+        'description': "list_python_repo_releases",
+
+        'arguments': "-p dls_testpythonmod -g",
+
+        'std_out_compare_string': python_list,
+
+    },
+
+    # List everything in ioc area
+    {
+        'description': "list_ioc_repo_releases",
+
+        'arguments': "-i BTEST/TS -g",
+
+        'std_out_compare_string': ioc_list,
+
+    },
 
 ]
 
 
 def test_generator():
-
-
 
     for test in st.generate_tests_from_dicts("dls-list-releases.py",
                                              st.SystemsTest,
