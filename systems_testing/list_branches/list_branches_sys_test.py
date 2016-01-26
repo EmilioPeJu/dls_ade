fe39e3ed450983176da4ab81c089f9a9c397b001
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-26 13:21:51 +0000

    Remove duplicate tests

--- systems_testing/list_branches/list_branches_sys_test.py
+++ systems_testing/list_branches/list_branches_sys_test.py
@@ -1,38 +1,16 @@
 from systems_testing import systems_testing as st
 
-support_list = "Branches of testsupportmod:\n\nbug-fix\nmaster\nnew-feature\n\n"
-python_list = "Branches of dls_testpythonmod:\n\nbug-fix\nmaster\nnew-feature\n\n"
-ioc_list = "Branches of BTEST/TS:\n\nbug-fix\nmaster\nnew-feature\n\n"
+branches_list = "Branches of dls_testpythonmod:\n\nbug-fix\nmaster\nnew-feature\n\n"
 
 settings_list = [
 
-    # List releases for support module on repo
-    {
-        'description': "list_support_branches",
-
-        'arguments': "testsupportmod",
-
-        'std_out_compare_string': support_list,
-
-    },
-
     # List releases for python module on repo
     {
-        'description': "list_python_branches",
+        'description': "list_branches",
 
         'arguments': "-p dls_testpythonmod",
 
-        'std_out_compare_string': python_list,
-
-    },
-
-    # List releases for ioc module on repo
-    {
-        'description': "list_ioc_branches",
-
-        'arguments': "-i BTEST/TS",
-
-        'std_out_compare_string': ioc_list,
+        'std_out_compare_string': branches_list,
 
     },
 
