b33ac4f62125f508073de5df58d692a88f91587f
GDYendell <gary.yendell@diamond.ac.uk>
2016-01-26 13:53:34 +0000

    Remove duplicate tests

--- systems_testing/module_contacts/module_contacts_sys_test.py
+++ systems_testing/module_contacts/module_contacts_sys_test.py
@@ -5,73 +5,17 @@ import shutil
 
 sys_test_dir_path = os.path.realpath(os.path.dirname(__file__))
 
-support_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
-                     'Performing search for lkz95212...\n' \
-                     'Performing search for mef65357...\n' \
-                     'testsupportmod,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
-
-python_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
-                     'Performing search for lkz95212...\n' \
-                     'Performing search for mef65357...\n' \
-                     'dls_testpythonmod,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
-
-ioc_csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
-                     'Performing search for lkz95212...\n' \
-                     'Performing search for mef65357...\n' \
-                     'BTEST/TS,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
+csv_output = 'Module,Contact,Contact Name,CC,CC Name\n' \
+             'Performing search for lkz95212...\n' \
+             'Performing search for mef65357...\n' \
+             'dls_testpythonmod,lkz95212,Martin Gaughran,mef65357,Gary Yendell\n'
 
 
 settings_list = [
 
-    # Check contacts of support module
-    {
-        'description': "check_support_contacts",
-
-        'arguments': "testsupportmod",
-
-        'std_out_compare_string': "Contact: lkz95212 (CC: mef65357)\n",
-
-    },
-
-    # Check contacts of support module in CSV format
-    {
-        'description': "check_support_contacts_CSV",
-
-        'arguments': "testsupportmod -s",
-
-        'std_out_compare_string': support_csv_output,
-
-    },
-
-    # Set contacts of support module
-    {
-        'description': "set_support_contacts",
-
-        'arguments': "testsupportmod -c mef65357 -d lkz95212",
-
-        'attributes_dict': {'module-contact': 'mef65357',
-                            'module-cc': 'lkz95212'},
-
-        'server_repo_path': "controlstest/support/testsupportmod",
-
-    },
-
-    # Set contacts of support module with CSV
-    {
-        'description': "set_support_contacts_CSV",
-
-        'arguments': "testsupportmod -m test_csv.txt",
-
-        'attributes_dict': {'module-contact': 'lkz95212',
-                            'module-cc': 'mef65357'},
-
-        'server_repo_path': "controlstest/support/testsupportmod",
-
-    },
-
     # Check contacts of python module
     {
-        'description': "check_python_contacts",
+        'description': "check_contacts",
 
         'arguments': "-p dls_testpythonmod",
 
@@ -81,17 +25,17 @@ settings_list = [
 
     # Check contacts of python module in CSV format
     {
-        'description': "check_python_contacts_CSV",
+        'description': "check_contacts_CSV",
 
         'arguments': "-p dls_testpythonmod -s",
 
-        'std_out_compare_string': python_csv_output,
+        'std_out_compare_string': csv_output,
 
     },
 
     # Set contacts of python module
     {
-        'description': "set_python_contacts",
+        'description': "set_contacts",
 
         'arguments': "-p dls_testpythonmod -c mef65357 -d lkz95212",
 
@@ -104,7 +48,7 @@ settings_list = [
 
     # Set contacts of python module with CSV
     {
-        'description': "set_python_contacts_CSV",
+        'description': "set_contacts_CSV",
 
         'arguments': "-p dls_testpythonmod -m python_test_csv.txt",
 
@@ -112,52 +56,6 @@ settings_list = [
                             'module-cc': 'mef65357'},
 
         'server_repo_path': "controlstest/python/dls_testpythonmod",
-
-    },
-
-    # Check contacts of ioc module
-    {
-        'description': "check_ioc_contacts",
-
-        'arguments': "-i BTEST/TS",
-
-        'std_out_compare_string': "Contact: lkz95212 (CC: mef65357)\n",
-
-    },
-
-    # Check contacts of ioc module CSV format
-    {
-        'description': "check_ioc_contacts",
-
-        'arguments': "-i BTEST/TS -s",
-
-        'std_out_compare_string': ioc_csv_output,
-
-    },
-
-    # Set contacts of ioc module
-    {
-        'description': "set_ioc_contacts",
-
-        'arguments': "-i BTEST/TS -c mef65357 -d lkz95212",
-
-        'attributes_dict': {'module-contact': 'mef65357',
-                            'module-cc': 'lkz95212'},
-
-        'server_repo_path': "controlstest/ioc/BTEST/TS",
-
-    },
-
-    # Set contacts of ioc module with CSV
-    {
-        'description': "set_ioc_contacts_CSV",
-
-        'arguments': "-i BTEST/TS -m ioc_test_csv.txt",
-
-        'attributes_dict': {'module-contact': 'lkz95212',
-                            'module-cc': 'mef65357'},
-
-        'server_repo_path': "controlstest/ioc/BTEST/TS",
 
     },
 
