7a6e1d2628be1aa6b0c01b50e6dad6a2c95cc842
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-02-03 14:52:04 +0000

    Add tests for exception and no releases done

--- system_testing/changes_since_release/changes_since_release_sys_test.py
+++ system_testing/changes_since_release/changes_since_release_sys_test.py
@@ -2,6 +2,7 @@ import system_testing as st
 
 changes = "Changes have been made to dls_testpythonmod since release 1-1\n"
 no_changes = "No changes have been made to dls_testpythonmod2 since most recent release 2-1\n"
+exception_msg = "Repository does not contain controlstest/python/testpythonmod"
 
 settings_list = [
 
@@ -22,6 +23,26 @@ settings_list = [
         'arguments': "-p dls_testpythonmod2",
 
         'std_out_compare_string': no_changes,
+
+    },
+
+    {
+        'description': "raise_exception_for_non_existent_module",
+
+        'arguments': "-p testpythonmod",
+
+        'exception_type': "Exception",
+
+        'exception_string': exception_msg,
+
+    },
+
+    {
+        'description': "no_releases_done",
+
+        'arguments': "-i testB06/TS",
+
+        'std_out_compare_string': "No release has been done for testB06/TS\n",
 
     },
 
