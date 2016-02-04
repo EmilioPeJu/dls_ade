423d283d258d5053ce295ffd9b3b350c1dd12816
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-02-04 10:39:19 +0000

    Make list_modules test work with specially designated area

--- system_testing/list_modules/list_modules_sys_test.py
+++ system_testing/list_modules/list_modules_sys_test.py
@@ -1,23 +1,17 @@
 import system_testing as st
+import os
 
 support_list = "Modules in support:\n\n" \
-               "testsupportmod\n" \
-               "testsupportmo\n"
+               "mock_repo/support/testsupportmod\n"
 
 ioc_list = "Modules in ioc:\n\n" \
-           "BTEST2/TS\n" \
-           "BTEST/BTEST-EB-IOC-03\n" \
-           "BTEST/BTEST-EB-IOC-0\n" \
-           "BTEST/BTEST-VA-IOC-04\n" \
-           "BTEST/TS\n" \
-           "testB06/TS\n" \
-           "testB07/TS\n"
+           "mock_repo/ioc/BTEST2/TS\n" \
+           "mock_repo/ioc/BTEST/BTEST-EB-IOC-03\n" \
+           "mock_repo/ioc/BTEST/TS\n"
 
 ioc_domain_list = "Modules in BTEST:\n\n" \
-                  "BTEST/BTEST-EB-IOC-03\n" \
-                  "BTEST/BTEST-EB-IOC-0\n" \
-                  "BTEST/BTEST-VA-IOC-04\n" \
-                  "BTEST/TS\n"
+                  "mock_repo/ioc/BTEST/BTEST-EB-IOC-03\n" \
+                  "mock_repo/ioc/BTEST/TS\n"
 
 settings_list = [
 
@@ -48,9 +42,40 @@ settings_list = [
 
 ]
 
+ORIGINAL_GIT_ROOT_DIR = os.getenv('GIT_ROOT_DIR')
+NEW_GIT_ROOT_DIR = ""
+
+
+def setup_module():
+    """Change environment variable and module variables.
+
+    """
+    global NEW_GIT_ROOT_DIR
+
+    # The environment variable is set for the use of the script being tested.
+    NEW_GIT_ROOT_DIR = "controlstest/targetOS/mock_repo"
+    os.environ['GIT_ROOT_DIR'] = NEW_GIT_ROOT_DIR
+
+    # Set so SystemTest object can use the new variable.
+    st.vcs_git.GIT_ROOT_DIR = NEW_GIT_ROOT_DIR
+    st.vcs_git.pathf.GIT_ROOT_DIR = NEW_GIT_ROOT_DIR
+
+
+def teardown_module():
+    """Change environment variable and module variables to the original values.
+
+    """
+    os.environ['GIT_ROOT_DIR'] = ORIGINAL_GIT_ROOT_DIR
+    st.vcs_git.GIT_ROOT_DIR = ORIGINAL_GIT_ROOT_DIR
+    st.vcs_git.pathf.GIT_ROOT_DIR = ORIGINAL_GIT_ROOT_DIR
+
 
 def test_generator():
+
+    setup_module()
 
     for test in st.generate_tests_from_dicts("dls-list-modules.py",
                                              settings_list):
         yield test
+
+    teardown_module()
