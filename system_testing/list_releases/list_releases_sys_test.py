78ec8e74c716312b0b8fcbc02aa40471dd28eeab
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-02-11 08:43:34 +0000

    Add an extra test for no release made in git

--- system_testing/list_releases/list_releases_sys_test.py
+++ system_testing/list_releases/list_releases_sys_test.py
@@ -2,7 +2,8 @@ import system_testing as st
 
 releases_list = "Previous releases for dls_testpythonmod2 in the repository:\n1-0\n1-1\n2-0\n2-1\n"
 latest_release = "The latest release for dls_testpythonmod2 in the repository is: 2-1\n"
-no_git_release = "Repository does not contain controlstest/python/testpythonmod"
+no_git_release = "testB06/TS: No releases made in git\n"
+module_does_not_exist = "Repository does not contain controlstest/python/testpythonmod"
 no_prod_release = "dummy2: No releases made for R3.14.12.3\n"
 prod_releases_list = "Previous releases for dummy in prod:\n0-3\n0-5\n0-6\n0-7\n0-8\n"
 latest_prod_release = "The latest release for dummy in prod is: 0-8\n"
@@ -30,13 +31,22 @@ settings_list = [
     },
 
     {
+        'description': "print_no_releases_made_for_a_module_on_the_repository",
+
+        'arguments': "-i testB06/TS -g",
+
+        'std_out_compare_string': no_git_release,
+
+    },
+
+    {
         'description': "raise_exception_for_a_non_existent_module_on_repository",
 
         'arguments': "-p testpythonmod -g",
 
         'exception_type': "dls_ade.exceptions.VCSGitError",
 
-        'exception_string': no_git_release,
+        'exception_string': module_does_not_exist,
 
     },
 
