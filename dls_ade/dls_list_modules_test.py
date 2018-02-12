df8d88644a1ccb95389a7647f4ee3c8a698ffbf8
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2018-02-12 11:46:14 +0000

    Fix broken unittest

--- dls_ade/dls_list_modules_test.py
+++ dls_ade/dls_list_modules_test.py
@@ -46,27 +46,22 @@ class PrintModuleListTest(unittest.TestCase):
 
         self.server_mock.get_server_repo_list.assert_called_once_with()
 
-    def test_given_valid_source_then_print_called(self):
+    def test_given_valid_source_then_list_of_modules(self):
         self.server_mock.get_server_repo_list.return_value =\
             ["test/source/module", "test/source2/module2"]
 
         source = "test/source"
 
-        with patch.object(builtins, 'print') as mock_print:
-            dls_list_modules.get_module_list(source)
+        module_list = dls_list_modules.get_module_list(source)
+        self.assertIsNotNone(module_list)
+        self.assertListEqual(module_list, ['module'])
 
-        call_args = mock_print.call_args_list
-        self.assertEqual(call_args[0][0][0], 'module')
-        # Check that module2 from source2 is not printed
-        self.assertEqual(len(call_args), 1)
-
-    def test_given_invalid_source_then_print_not_called(self):
+    def test_given_invalid_source_then_empty_list_of_modules(self):
         self.server_mock.get_server_repo_list.return_value = \
             ["test/not_source/module"]
 
         source = "test/source"
 
-        with patch.object(builtins, 'print') as mock_print:
-            dls_list_modules.get_module_list(source)
-
-        self.assertFalse(mock_print.call_count)
+        module_list = dls_list_modules.get_module_list(source)
+        self.assertIsNotNone(module_list)
+        self.assertListEqual(module_list, [])
