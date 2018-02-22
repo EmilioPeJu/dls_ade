7e5977f25025fe46d9deb7b61f342746d5649e61
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2018-02-22 12:16:46 +0000

    dlsbuild epics: adding logging

--- dls_ade/dlsbuild_scripts/Linux/epics.sh
+++ dls_ade/dlsbuild_scripts/Linux/epics.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 # ******************************************************************************
 # 
@@ -27,6 +27,7 @@ export PATH
 case $_module in
     base)
         build_dir=${_build_dir}/${_epics%%_64}
+        module load controls-tools  #  Required to get the tools util-linux/logger on the path
         ;;
     extensions)
         build_dir=${_build_dir}/${_epics%%_64}
@@ -40,26 +41,31 @@ case $_module in
         ;;
 esac
 
+SysLog debug "Creating dir: " $build_dir
 mkdir -p $build_dir                         || ReportFailure "Can not mkdir $build_dir"
 cd $build_dir                               || ReportFailure "Can not cd to $build_dir"
 
 # If force, remove existing version directory (whether or not it exists)
 if [ "$_force" == "true" ] ; then
-    rm -rf $_module                           || ReportFailure "Could not rm $_module"
+    SysLog info "Force: removing previous version: " ${PWD}/$_module
+   rm -rf $_module                           || ReportFailure "Could not rm $_module"
 fi
 
 # Clone module if it doesn't already exist
 if [ ! -d $_module ]; then
+    SysLog info "Cloning repo: $_git_dir into: $_module"
     git clone --depth=100 $_git_dir $_module                            || ReportFailure "Can not clone $_git_dir"
 fi
 
 # Checkout module
 cd $_module                                                         || ReportFailure "Could not cd to $_module"
+SysLog info "checkout version tag: " $_version
 git fetch --depth=1 origin tag $_version && git checkout $_version  || ReportFailure "Could not checkout $_version"
 
 # Build
 error_log=${_build_name}.err
 build_log=${_build_name}.log
+SysLog info "Starting build. Build log: ${PWD}/${build_log} errors: ${PWD}/${error_log}"
 {
     {
         make clean && make
@@ -73,3 +79,5 @@ if (( $(cat ${_build_name}.sta) != 0 )) ; then
 elif (( $(stat -c%s $error_log) != 0 )) ; then
     cat $error_log | mail -s "Build Errors: $_area $_module $_version" $_email
 fi
+
+SysLog info "Build complete"
