1eb45f301e3cb72dd2a0fb4c0951c29217825c51
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2018-02-26 22:40:16 +0000

    dlsbuild util template logger function use descriptive variables to define syslog server and port

--- dls_ade/dlsbuild_scripts/Linux/utils_template.sh
+++ dls_ade/dlsbuild_scripts/Linux/utils_template.sh
@@ -5,15 +5,26 @@
 #     2) Message
 SysLog()
 {
-    echo ${@:2} |
-    logger \
+    local dls_syslog_server=cs03r-sc-serv-30
+    local dls_syslog_server_port=5150
+
+    local syslog_level_str=$1
+    local syslog_message=${@:2}
+
+    # Send the log message somewhere.
+    # Fail safe implmented so that different logging mechanisms are tried in order, catching failures:
+    #    1: dls-logger (requires module controls-tools to be pre-loaded)
+    #    2: netcat (nc)
+    #    3:
+    echo ${syslog_message} |
+    dls-logger \
     --tag dcs_build_job-$(uname -m) \
     -s -d -p local2.$1 \
-    -n cs03r-sc-serv-30 -P 5150 \
+    -n ${dls_syslog_server} -P ${dls_syslog_server_port} \
     --rfc5424 \
     --sd-id dcs@32121 \
       --sd-param build_job_parameters=\"build_name=$_build_name\ area=$_area\ module=$_module\ version=$_version\ email=$_email\ username=$_user\" \
-    || echo "Build server error. logger failed:" $_build_name
+    || echo "(logger failed) Build server error:" $_build_name ${syslog_message}
 }
 
 ReportFailure()
