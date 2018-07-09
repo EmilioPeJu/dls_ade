b571fba2bfddc54aec81ea296770c23ba17b561f
Sky French <sky.french@diamond.ac.uk>
2018-07-09 09:29:06 +0100

    adding unittests for tag_utility

--- dls_ade/tag_utility.py
+++ dls_ade/tag_utility.py
@@ -1,10 +1,11 @@
 import re
 from dls_ade.exceptions import ParsingError
+from dls_ade.vcs_git import create_tag_and_push
 
 
 def check_tag_is_valid(tag):
     """
-    Checks if a given tag is avalid tag.
+    Checks if a given tag is a valid tag.
 
     Args:
         tag(str): proposed tag string
@@ -17,5 +18,17 @@ def check_tag_is_valid(tag):
     check = re.compile('[0-9]+\-[0-9]+(\-[0-9]+)?(dls[0-9]+(\-[0-9]+)?)?')
     result = check.match(tag)
 
+    if result is None:
+        raise ParsingError("Invalid tag " + tag)
+
     if result.group() != tag:
         raise ParsingError("Invalid tag " + tag)
+
+
+def tag_commit(repo, tag, commit_ref):
+    """
+    Creates a tag in the local repository.
+    Pushes this tag to the remote.
+    """
+
+    create_tag_and_push(repo, tag, commit_ref)
