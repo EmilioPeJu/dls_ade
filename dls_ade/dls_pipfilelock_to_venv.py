1bbb187d40daa90aee7d7cfc67ca6c8a198032b0
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2018-07-12 10:04:38 +0100

    final version of pipfile_to_venv script only produces path file if all packages are there, if not produces a list of packages that are not installed

--- dls_ade/dls_pipfilelock_to_venv.py
+++ dls_ade/dls_pipfilelock_to_venv.py
@@ -1,21 +1,54 @@
 import sys
 import os
+import os.path
 import venv
 from collections import OrderedDict
 from git import Repo
 import json
 
 
+TESTING_ROOT = '/home/svz41317/testing-root'
+
+
 def main():
  
-    with open('Pipfile.lock') as f:
-        j = json.load(f, object_pairs_hook=OrderedDict)
-        packages = OrderedDict(j['default'])
-        packages.update(j['develop'])
- 
-    with open('./venv/lib/python3.6/site-packages/paths.pth', 'w') as f:
-        for package, contents in packages.items():
-            f.write('/home/svz41317/testing-root/dls_sw/prod/python3/RHEL6-x86_64/{}/{}/prefix/lib/python3.6/site-packages\n'.format(package, contents['version'][2:]))
+    try:
+    	with open('Pipfile.lock') as f:
+            j = json.load(f, object_pairs_hook=OrderedDict)
+            packages = OrderedDict(j['default'])
+            packages.update(j['develop'])
+    except FileNotFoundError:
+        sys.exit('Pipfile.lock was not found!')
+        
+    if not os.path.exists('venv'):
+        venv.create('venv',system_site_packages=False, clear=False, symlinks=False, with_pip=False)
+    else:
+        sys.exit('venv already present!')
+
+    path_list = []
+    pkg_not_found = False
+    absent_pkg_list = []
+    
+    for package, contents in packages.items():
+        version_string = contents['version']
+        assert version_string.startswith('==')
+        version = version_string[2:]
+        file_path='{}/dls_sw/prod/python3/RHEL6-x86_64/{}/{}/prefix/lib/python3.6/site-packages'.format(TESTING_ROOT, package, version)
+        path_list.append(file_path)
+
+    for p in path_list:
+        if not os.path.exists(p):
+            pkg_not_found=True
+            absent_pkg_list.append(p)
+
+    if not pkg_not_found:
+        with open('./venv/lib/python3.6/site-packages/paths.pth', 'w') as f:
+            for pl in path_list:
+                f.write(pl +'\n')
+    else:
+        print('The following packages need to be installed:')
+        print(*absent_pkg_list, sep='\n')
+        print("¯\_(ツ)_/¯")
 
 
-    venv.create('venv',system_site_packages=False, clear=False, symlinks=False, with_pip=False)
+    
