8274fa51d8df7d7e379e5c550a9ec6992309b3be
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2018-08-06 12:33:59 +0100

    removed refs to home folders, set TESTING_ROOT as env variable

--- dls_ade/dls_populate_dist.py
+++ dls_ade/dls_populate_dist.py
@@ -1,15 +1,19 @@
 """
-Get all the package wheels from PyPI and into a wheel cache under /work
-Subprocesses are used deliberatelly as the reccommended way of executing pip programmatically according to pip manual(pip 10.0.1) 
+Get all the packages from PyPI and into a wheel cache under /dls_sw/work.
+If a tar file is downloaded, this script will build a wheel.
+Subprocesses are used deliberatelly as the reccommended way of executing pip 
+programmatically according to pip manual(pip 10.0.1) 
 """
 
 from collections import OrderedDict
 import json
 import subprocess
 import sys
+import os
 
-TESTING_ROOT = '/home/svz41317/testing-root'
-work_wheel_dir = TESTING_ROOT+'/dls_sw/work/python3/distributions' 
+
+TESTING_ROOT = os.getenv('TESTING_ROOT', "")
+work_wheel_dir = TESTING_ROOT + '/dls_sw/work/python3/distributions' 
 
 
 def main():
@@ -21,8 +25,8 @@ def main():
             packages.update(j['develop'])
             for package, contents in packages.items():
                 version = contents['version']
-                subprocess.check_call([sys.executable,'-m','pip','wheel','--no-deps','--wheel-dir='+work_wheel_dir,package+version])    
+                specifier = package + version  # example: flask==1.0.2
+                subprocess.check_call([sys.executable, '-m', 'pip', 'wheel', '--no-deps', '--wheel-dir=' + work_wheel_dir, specifier])    
     except FileNotFoundError:
         sys.exit('Job aborted: Pipfile.lock was not found!')
-  
-        
+
