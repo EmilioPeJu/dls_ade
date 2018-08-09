a1de228a73b844ec575da0a9f9eaa12af8c11067
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2018-08-09 14:17:47 +0100

    style changes on pipfilelock-to-venv.py

--- dls_ade/dls_pipfilelock_to_venv.py
+++ dls_ade/dls_pipfilelock_to_venv.py
@@ -6,7 +6,6 @@ import sys
 import os.path
 import venv
 from collections import OrderedDict
-from git import Repo
 import json
 
 
@@ -16,13 +15,12 @@ TESTING_ROOT = os.getenv('TESTING_ROOT', "")
 def main():
  
     try:
-    	with open('Pipfile.lock') as f:
+        with open('Pipfile.lock') as f:
             j = json.load(f, object_pairs_hook=OrderedDict)
             packages = OrderedDict(j['default'])
             packages.update(j['develop'])
     except FileNotFoundError:
         sys.exit('Job aborted: Pipfile.lock was not found!')
-
 
     path_list = []
     pkg_not_found = False
@@ -32,28 +30,26 @@ def main():
         version_string = contents['version']
         assert version_string.startswith('==')
         version = version_string[2:]
-        file_path='{}/dls_sw/prod/python3/RHEL6-x86_64/{}/{}/prefix/lib/python3.6/site-packages'.format(TESTING_ROOT, package, version)
+        file_path = '{}/dls_sw/prod/python3/RHEL6-x86_64/{}/{}/prefix/lib/python3.6/site-packages'.format(TESTING_ROOT,
+                                                                                                          package,version)
         path_list.append(file_path)
 
     for p in path_list:
         if not os.path.exists(p):
-            pkg_not_found=True
+            pkg_not_found = True
             absent_pkg_list.append(p)
 
     if not pkg_not_found:
         
         if not os.path.exists('venv'):
-            venv.create('venv',system_site_packages=True, clear=False, symlinks=False, with_pip=False)
+            venv.create('venv', system_site_packages=True, clear=False, symlinks=False, with_pip=False)
         else:
             sys.exit('venv already present!')
         with open('./venv/lib/python3.6/site-packages/paths.pth', 'w') as f:
             for pl in path_list:
-                f.write(pl +'\n')
+                f.write(pl + '\n')
         print('venv with path.pth has been created successfully!')
     else:
         print('The following packages need to be installed:')
         print(*absent_pkg_list, sep='\n')
         print("¯\_(ツ)_/¯")
-
-
-    
