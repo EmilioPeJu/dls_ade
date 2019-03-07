51bcf80aaebb77481c149446cdc8de81ab2a7db8
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2019-03-07 11:10:46 +0000

    Point to different dls_ade and python

--- dls_ade/dlsbuild_scripts/Linux/python3lib.sh
+++ dls_ade/dlsbuild_scripts/Linux/python3lib.sh
@@ -23,19 +23,22 @@
 
 
 # Testing section, this will need correcting for the final version.
-PYTHON=~/python3/bin/python3
+#PYTHON=~/python3/bin/python3
 OS_VERSION=RHEL$(lsb_release -sr | cut -d. -f1)-$(uname -m)
+PYTHON=/dls_sw/work/tools/${OS_VERSION}/Python3/prefix/bin/dls-python3
 PYTHON_VERSION="python$($PYTHON -V | cut -d" " -f"2" | cut -d"." -f1-2)"
-DLS_ADE_LOCATION=~/dls_ade
+#DLS_ADE_LOCATION=~/dls_ade
+DLS_ADE_LOCATION=/dls_sw/work/python3/${OS_VERSION}/dls_ade
 PFL_TO_VENV=${DLS_ADE_LOCATION}/prefix/bin/dls-pipfilelock-to-venv.py
-export PYTHONPATH=${DLS_ADE_LOCATION}/prefix/lib/python3.6/site-packages
+export PYTHONPATH=${DLS_ADE_LOCATION}/prefix/lib/$PYTHON_VERSION/site-packages
 
 export TESTING_ROOT=~/testing-root
+#export TESTING_ROOT=/dls_sw/work/python3/test-root
 CENTRAL_LOCATION=$TESTING_ROOT/dls_sw/prod/python3/$OS_VERSION
 WORK_DIST_DIR=$TESTING_ROOT/dls_sw/work/python3/distributions
 PROD_DIST_DIR=$TESTING_ROOT/dls_sw/prod/python3/distributions
 
-export PATH=~/python3/bin:$PATH
+export PATH=/dls_sw/work/tools/${OS_VERSION}/Python3/prefix/bin:$PATH
 
 
 # Copy dist (and lockfile if there is one) from work to prod folder
