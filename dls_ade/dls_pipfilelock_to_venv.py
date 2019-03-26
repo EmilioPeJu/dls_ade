de42854107e4b3301a3f6f410275c5be815672d9
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2019-03-26 14:17:02 +0000

    refactoring

--- dls_ade/dls_pipfilelock_to_venv.py
+++ dls_ade/dls_pipfilelock_to_venv.py
@@ -18,6 +18,42 @@ OS_VERSION = default_server().replace('redhat', 'RHEL')
 PYTHON_VERSION = f'python{sys.version_info[0]}.{sys.version_info[1]}'
 OS_DIR = f'{TESTING_ROOT}/dls_sw/prod/python3/{OS_VERSION}'
 
+path_list = []
+absent_pkg_list = []
+
+
+def construct_pkg_path(_packages):
+    for package, contents in _packages.items():
+        version_string = contents['version']
+        assert version_string.startswith('==')
+        version = version_string[2:]
+        file_path = '{}/{}/{}/prefix/lib/{}/site-packages'.format(
+            OS_DIR, package, version, PYTHON_VERSION
+        )
+        path_list.append(file_path)
+
+def find_missing_pkgs():
+    for p in path_list:
+        if not os.path.exists(p):
+            absent_pkg_list.append(p)
+
+def create_venv():
+    if not absent_pkg_list:
+
+        if not os.path.exists('lightweight-venv'):
+            venv.create('lightweight-venv', system_site_packages=True, clear=False,
+                        symlinks=False, with_pip=False)
+        else:
+            sys.exit('lightweight-venv already present!')
+        paths_file = f'./lightweight-venv/lib/{PYTHON_VERSION}/site-packages/dls-installed-packages.pth'
+        with open(paths_file, 'w') as f:
+            for path in path_list:
+                f.write(path + '\n')
+        print('lightweight-venv with dls-installed-packages.pth has been created successfully!')
+    else:
+        print('The following packages need to be installed:')
+        print('\n'.join(absent_pkg_list))
+        sys.exit(1)
 
 def main():
 
@@ -30,35 +66,6 @@ def main():
     except IOError:
         sys.exit('Job aborted: Pipfile.lock was not found!')
 
-    path_list = []
-    absent_pkg_list = []
-    
-    for package, contents in packages.items():
-        version_string = contents['version']
-        assert version_string.startswith('==')
-        version = version_string[2:]
-        file_path = '{}/{}/{}/prefix/lib/{}/site-packages'.format(
-            OS_DIR, package, version, PYTHON_VERSION
-        )
-        path_list.append(file_path)
-
-    for p in path_list:
-        if not os.path.exists(p):
-            absent_pkg_list.append(p)
-
-    if not absent_pkg_list:
-        
-        if not os.path.exists('lightweight-venv'):
-            venv.create('lightweight-venv', system_site_packages=True, clear=False,
-                                        symlinks=False, with_pip=False)
-        else:
-            sys.exit('lightweight-venv already present!')
-        paths_file = f'./lightweight-venv/lib/{PYTHON_VERSION}/site-packages/dls-installed-packages.pth'
-        with open(paths_file, 'w') as f:
-            for path in path_list:
-                f.write(path + '\n')
-        print('lightweight-venv with dls-installed-packages.pth has been created successfully!')
-    else:
-        print('The following packages need to be installed:')
-        print('\n'.join(absent_pkg_list))
-        sys.exit(1)
+    construct_pkg_path(packages)
+    find_missing_pkgs()
+    create_venv()
