2e7ac4c09dd759f682efbff0b51f850b75019b1d
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2019-04-01 16:01:40 +0100

    minor corrections

--- dls_ade/dls_populate_dist_test.py
+++ dls_ade/dls_populate_dist_test.py
@@ -1,7 +1,7 @@
 #!/bin/env dls-python
 
 import unittest
-from dls_ade import dls_populate_dist as pdist
+from dls_ade import dls_populate_dist
 import os
 
 class PopulateDist(unittest.TestCase):
@@ -11,19 +11,19 @@ class PopulateDist(unittest.TestCase):
 
     def test_conversion_from_dash_to_underscore_is_ok(self):
         package, version = 'module-name', '1-2-3'
-        test_list = pdist.format_pkg_name(package, version)
+        test_list = dls_populate_dist.format_pkg_name(package, version)
         test_module_name = test_list.split()[0]
         self.assertEqual(test_module_name, 'module_name')
 
     def test_ensure_missing_pkgs_list_correctly_filled_with_items(self):
         package, version = 'module-name', '==1-2-3'
-        test_item = pdist.format_pkg_name(package, version)
+        test_item = dls_populate_dist.format_pkg_name(package, version)
         self.assertEqual(test_item, 'module_name 1-2-3')
 
     def test_ensure_wheel_is_downloaded(self):
         test_lockfile_path = '/dls_sw/work/python3/unit-testing'
         os.chdir(test_lockfile_path)
-        pdist.populate_dist()
+        dls_populate_dist.populate_dist()
         myfiles = os.listdir('/dls_sw/work/python3/distributions')
         target_wheel = 'numpy-1.16.2-cp37-cp37m-manylinux1_x86_64.whl'
         assert target_wheel in myfiles
@@ -31,5 +31,5 @@ class PopulateDist(unittest.TestCase):
     def test_populate_dist_returns_item(self):
         test_lockfile_path = '/dls_sw/work/python3/unit-testing'
         os.chdir(test_lockfile_path)
-        commands = pdist.populate_dist()
-        assert commands[0] == 'numpy 1.16.2'
+        item = dls_populate_dist.populate_dist()
+        self.assertEqual(item[0], 'numpy 1.16.2')
