556764049db7718e6c42bec3c2123b9606705bde
Andy Wilson <andrew.wilson@diamond.ac.uk>
2019-04-11 09:02:28 +0100

    A couple of tests for creating empty remote

--- dls_ade/dls_start_new_module_test.py
+++ dls_ade/dls_start_new_module_test.py
@@ -4,6 +4,7 @@ import unittest
 import dls_ade.dls_start_new_module
 from argparse import _StoreTrueAction
 from mock import patch
+from dls_ade.exceptions import VerificationError
 
 
 class MakeParserTest(unittest.TestCase):
@@ -29,6 +30,37 @@ class MakeParserTest(unittest.TestCase):
         self.assertIsInstance(option, _StoreTrueAction)
         self.assertEqual(option.dest, "fullname")
         self.assertIn("--fullname", option.option_strings)
+
+    @patch("dls_ade.dls_start_new_module.Server.create_remote_repo")
+    @patch("dls_ade.dls_start_new_module.Server.is_server_repo")
+    def test_create_empty_project_name_taken(self,
+                                             mock_is_server_repo,
+                                             mock_create_remote_repo):
+
+        # Repo already exists
+        mock_is_server_repo.return_value = True
+
+        # Raise exception and do not try to create the repo
+        with self.assertRaises(VerificationError):
+            dls_ade.dls_start_new_module.create_empty_remote("ioc",
+                                                             "BL99P-TS-IOC-03")
+        mock_create_remote_repo.assert_not_called()
+
+    @patch("dls_ade.dls_start_new_module.Server.create_remote_repo")
+    @patch("dls_ade.dls_start_new_module.Server.is_server_repo")
+    def test_create_empty_project_name_avalable(self,
+                                                mock_is_server_repo,
+                                                mock_create_remote_repo):
+        # Repo does not already exist
+        mock_is_server_repo.return_value = False
+
+        dls_ade.dls_start_new_module.create_empty_remote("ioc",
+                                                         "BL99P-TS-IOC-03")
+        mock_create_remote_repo.assert_called_once()
+
+
+    def test_create_empty_project_quiet(self):
+        pass
 
 
 if __name__ == '__main__':
